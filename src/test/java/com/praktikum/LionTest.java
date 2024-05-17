02465afb46c6471864332699c508d7430c407ecd
Elena Tikhanova <kica85@yandex.ru>
2024-05-17 13:50:13 +0400

    update develop

--- src/test/java/com/praktikum/LionTest.java
+++ src/test/java/com/praktikum/LionTest.java
@@ -1,2 +1,41 @@
-package com.praktikum;public class LionTest {
+package com.praktikum;
+
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class LionTest {
+    private Lion lion;
+    @Mock
+    private Feline feline;
+
+    @Test
+    public void testGetKittens() throws Exception {
+        lion = new Lion("Самец",feline);
+        lion.getKittens();
+        Mockito.verify(feline).getKittens();
+    }
+
+    @Test
+    public void testGetFood() throws Exception {
+        lion = new Lion("Самец",feline);
+        lion.getFood();
+        Mockito.verify(feline).getFood(Mockito.anyString());
+    }
+
+    @Test
+    public void testExceptionAndCheckText() {
+        feline = new Feline();
+        Exception exception = Assert.assertThrows(Exception.class, () -> {
+            new Lion("Неизвестно",feline);
+        });
+        Assert.assertEquals("Используйте допустимые значения пола животного - самец или самка", exception.getMessage());
+    }
 }
